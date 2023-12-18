11e860830669de67ab310b0f3795cab9a7b03084
Saaed Darwish <saaeddarwish1@gmail.com>
2023-12-18 06:53:43 -0700

    progress

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
--- src/Home.js
+++ src/Home.js
@@ -1,48 +1,39 @@
 import React from 'react';
-import { 
-  Button,
-  Container,
-  Row,
-  Col,
-  Navbar,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink
-} from 'reactstrap';
+import { Container, Row, Col, Navbar, NavbarBrand } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  return (
-    <div>
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">STRONGRIDGE DEVELOPMENTS</NavbarBrand>
-        <Nav className="ml-auto" navbar>
-          <NavItem>
-            <NavLink href="/components/">Components</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-          </NavItem>
-          <Button color="primary">REQUEST A QUOTE</Button>
-        </Nav>
-      </Navbar>
-      <Container>
-        <Row>
-          <Col>
-            <h1>Discover Home Remodeling Excellence</h1>
-            <p>Your Dream Home Awaits, Let's Make It Happen</p>
-            <Button color="secondary">GET A FREE ESTIMATE</Button>
-          </Col>
-          <Col>
-            {/* Content for the right side of the page, such as images or additional text */}
-          </Col>
-        </Row>
-      </Container>
-      <footer>
-        {/* Footer content here */}
-      </footer>
-    </div>
-  );
+    return (
+        <div>
+            <Navbar color="faded" light style={{ width: "100%", position: "fixed", top: "0", display: "flex", justifyContent: "center", alignItems: "center", zIndex: "1000"}}>
+                <NavbarBrand tag={Link} to="/"><img src='https://t3.ftcdn.net/jpg/00/79/67/48/360_F_79674837_Jn4HbqZGciL1jMeobbM6xNOUUjen936J.jpg' style={{height: "75px", width: "250px", }} alt='Divergent Smart Builds'></img></NavbarBrand>
+                <button className='Estimate-Button'>Request A Quote</button>
+            </Navbar>
+            <img src='https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?cs=srgb&dl=pexels-binyamin-mellish-106399.jpg&fm=jpg' style={{ marginTop: "100px", height: "750px", width: "100%", position: "relative" }}></img>
+            <Container style={{position: "absolute", top: "150px"}}>
+                <Row>
+                    <Col>
+                        <h1>Discover Home Remodeling Excellence</h1>
+                        <p>Your Dream Home Awaits, Let's Make It Happen</p>
+                        <button color="secondary">GET A FREE ESTIMATE</button>
+                    </Col>
+                </Row>
+            </Container>
+            <footer>
+                <Container>
+                    <Row>
+                        <p>footer</p>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <p>extra</p>
+                    </Row>
+                </Container>
+            </footer>
+        </div>
+    );
 };
 
 export default Home;
